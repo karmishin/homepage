e2dc83da810c7c4d8a1cdfda1d72edb394136479
Mikhail Karmishin <mikhail@karmishin.com>
2024-03-27 00:13:10 +0300

    Move everything into main.go for now

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"errors"
 	"flag"
 	"html/template"
 	"log"
@@ -58,6 +59,21 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Add("content-type", "text/plain")
 	http.Redirect(w, r, searchUrl, http.StatusFound)
+}
+
+func getSearchUrl(query string, engine string) (string, error) {
+	switch engine {
+	case "google":
+		return "https://google.com/search?q=" + query, nil
+	case "duckduckgo":
+		return "https://duckduckgo.com/?q=" + query, nil
+	case "marginalia":
+		return "https://search.marginalia.nu/search?query=" + query, nil
+	case "hn":
+		return "https://google.com/search?q=site:news.ycombinator.com+" + query, nil
+	default:
+		return "", errors.New("unknown engine")
+	}
 }
 
 func main() {
